c6fbc14fb310375e2c1917b79f138f40d0dcb468
Zhanat <janat.200066@gmail.com>
2023-09-06 05:38:26 +0600

    add sentry

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import re_path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -17,6 +18,22 @@ schema_view = get_schema_view(
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
+)
+
+sentry_sdk.init(
+    dsn="https://36ad42c140b97b4988665865362f23d2@o1321541.ingest.sentry.io/4505830483951616",
+    integrations=[DjangoIntegration()],
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
 )
 
 
